bcdf57e8d8d3044ebcbca4c11283785c87bf6730
unknown <Steph@Steph-PC.(none)>
2012-04-15 20:28:56 -0400

    Linked above and inside

--- inside/script.js
+++ inside/script.js
@@ -6,13 +6,15 @@ var curnum;
 var curind=0;
 
 $(function() {
-    for(var i=1; i<6; i++){
+	var floors2 = getUrlVars()['floors'].split('x');
+    for(var i=0; i < floors2.length; i++){
         var floor=$("<div></div>");
         floor.addClass("floor");
-        var num=Math.round(Math.random()*3)+i*3;
+        /* var num=Math.round(Math.random()*3)+i*3;
         if(target-i*3<3 && target>i*3){
             num=target;
-        }
+        } */
+        var num = floors2[i];
         floor.html(num.toString());
         floors.push(floor);
         $("#floorlist").prepend(floor);
@@ -33,4 +35,16 @@ countdown=function(){
         curfloor=floors[curind];
         curnum=curfloor.html();
     }
+}
+
+/* gets the variables in the url */
+function getUrlVars(){
+	var vars = [], hash;
+	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	for(var i = 0; i < hashes.length; i++){
+		hash = hashes[i].split('=');
+		vars.push(hash[0]);
+		vars[hash[0]] = hash[1];
+	}
+	return vars;
 }
